de03266fcf610e300ce82b35ebcabf345a3069bc
Erik Rivera <erik.river@gmail.com>
2011-09-09 20:00:33 -0500

    new url pattern

--- socialapps/cms/urls.py
+++ socialapps/cms/urls.py
@@ -1,7 +1,15 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 from socialapps.cms.views import *
 
+if settings.APPEND_SLASH:
+    base = url( r'^(?P<path>[0-9A-Za-z-_.//]+)/$' , BaseContentView.as_view(), name="base_view"),
+else:
+    base = url( r'^(?P<path>[0-9A-Za-z-_.//]+)$' , BaseContentView.as_view(), name="base_view"),
+
 urlpatterns = patterns('',
-    url(r'^(?P<path>.*)$', CMSBaseView.as_view(), name="base_view"),
+    base,
+    url( r'^(?P<path>[0-9A-Za-z-_.//]+)/edit$' , BaseContentEdit.as_view(), name="base_edit"),
+    url( r'^(?P<path>[0-9A-Za-z-_.//]+)/add/(?P<portal_type>)$' , BaseContentEdit.as_view(), name="base_add"),
 )
