d5a7bb86d32ecb4178146192268f5628adbf456f
Isaac I. Torres <istodi@gmail.com>
2011-10-12 16:14:24 -0500

    design 0.1 for cms types

--- socialapps/cms/forms.py
+++ socialapps/cms/forms.py
@@ -1,32 +1,51 @@
 from django import forms
 from .models import *
+from socialapps.core.widgets import RichTextEditor
 
 class BaseContentEditForm(forms.ModelForm):
     class Meta:
         model = BaseContent
         fields = ("title", "description", "tags")
+        widgets = {
+            'description' : RichTextEditor,
+        }
 
 class FolderEditForm(forms.ModelForm):
     class Meta:
         model = Folder 
         fields = ("title", "description", "tags")
+        widgets = {
+            'description' : RichTextEditor,
+        }
         
 class MultiPageEditForm(forms.ModelForm):
     class Meta:
         model = MultiPage 
-        fields = ("title", "description", "tags")
+        fields = ("title", "description", "tags", "show_toc")
+        widgets = {
+            'description' : RichTextEditor,
+        }
 
 class PageEditForm(forms.ModelForm):
     class Meta:
         model = Page 
         fields = ("title", "description", "tags")
+        widgets = {
+            'description' : RichTextEditor,
+        }
         
 class ImageEditForm(forms.ModelForm):
     class Meta:
         model = Image 
         fields = ("title", "description", "tags", "image")
+        widgets = {
+            'description' : RichTextEditor,
+        }
         
 class FileEditForm(forms.ModelForm):
     class Meta:
         model = File 
-        fields = ("title", "description", "tags", "file")        
+        fields = ("title", "description", "tags", "file")
+        widgets = {
+            'description' : RichTextEditor,
+        }    
