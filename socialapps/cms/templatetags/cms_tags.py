a3f20dc4fbb38682b4bf947b6741b8d4385acf70
Isaac I. Torres <istodi@gmail.com>
2012-01-19 18:27:23 -0600

    portlet menu de cms

--- socialapps/cms/templatetags/cms_tags.py
+++ socialapps/cms/templatetags/cms_tags.py
@@ -3,6 +3,13 @@ import re
 
 register = template.Library()
 
+@register.inclusion_tag("cms/navigation_portlet.html")
+def show_navigation(obj):
+    if not hasattr(obj,'get_local_menu'):
+        while not hasattr(obj,'get_local_menu'):
+            obj = obj.parent.get_type_object()
+    return { 'menu' : obj.get_local_menu(), 'absolute_url': obj.get_absolute_url()}
+
 @register.simple_tag(takes_context=True)
 def get_content_parent(context, obj):
     for item in obj.get_ancestors(include_self=True, ascending=True):
