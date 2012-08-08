7689882035349e268803048366a4b36764faa9f4
Isaac Torres <istodi@gmail.com>
2012-08-08 15:34:43 -0500

    new icons

--- socialapps/cms/types.py
+++ socialapps/cms/types.py
@@ -10,19 +10,19 @@ class FileTemplate(PortalTemplate):
     title = _('File')
     path = 'cms/file.html'
     image = '/static/images/types/file.png'
-    
+
 class ImageTemplate(PortalTemplate):
     name = 'image'
     title = _('Image')
     path = 'cms/image.html'
     image = '/static/images/types/image.png'
-    
+
 class FolderTemplate(PortalTemplate):
     name = 'folder'
     title = _('Folder')
     path = 'cms/folder.html'
     image = '/static/images/types/folder.png'
-    
+
 class FolderAdminTemplate(PortalTemplate):
     name = 'folder_admin'
     title = _('Folder Admin')
@@ -40,43 +40,43 @@ class FolderContentTemplate(PortalTemplate):
     title = _('Folder Content')
     path = 'cms/folder_content.html'
     image = '/static/images/types/folder_content.png'
-    
+
 class PageAdminTemplate(PortalTemplate):
     name = 'page_admin'
     title = _('Page Admin')
     path = 'cms/page_admin.html'
     image = '/static/images/types/page.png'
-    
+
 class PageTemplate(PortalTemplate):
     name = 'page'
     title = _('Page')
     path = 'cms/page.html'
     image = '/static/images/types/page.png'
-    
+
 class MultiPageAdminTemplate(PortalTemplate):
     name = 'multipage_admin'
     title = _('Multi Page Admin')
     path = 'cms/multipage_admin.html'
-    image = '/static/images/types/multipage.png'    
+    image = '/static/images/types/multipage.png'
 
 class MultiPageTemplate(PortalTemplate):
     name = 'multipage'
     title = _('Multi Page')
     path = 'cms/multipage.html'
     image = '/static/images/types/multipage.png'
-    
+
 #=============== Types Definition ===================
 
 class FileType(PortalType):
     name = "file"
     title = _("File")
     global_addable = False
-    icon = 'images/file-32.png'
+    icon = 'images/icons/32x32/save.png'
     subtypes = []
     templates = [FileTemplate,]
     default_template = FileTemplate
     add_form = FileEditForm
-    edit_form = FileEditForm    
+    edit_form = FileEditForm
 
 class ImageType(PortalType):
     name = "image"
@@ -87,13 +87,13 @@ class ImageType(PortalType):
     templates = [ImageTemplate,]
     default_template = ImageTemplate
     add_form = ImageEditForm
-    edit_form = ImageEditForm    
+    edit_form = ImageEditForm
 
 class PageType(PortalType):
     name = "page"
     title = _("Page")
     global_addable = False
-    icon = 'images/page-32.png'
+    icon = 'images/icons/32x32/page.png'
     subtypes = [ImageType, FileType,]
     templates = [PageTemplate, PageAdminTemplate]
     default_template = PageTemplate
@@ -115,13 +115,13 @@ class FolderType(PortalType):
     name = 'folder'
     title = _('Folder')
     global_addable = True
-    icon = 'images/folder-32.png'
+    icon = 'images/icons/32x32/folder.png'
     subtypes = [PageType, MultiPageType, FileType, ImageType]
     templates = [FolderTemplate, FolderAdminTemplate, FolderTableTemplate, FolderContentTemplate]
     default_template = FolderTemplate
     add_form = FolderEditForm
     edit_form = FolderEditForm
-    
+
 portal_types.registry(File, FileType)
 portal_types.registry(Image, ImageType)
 portal_types.registry(Page, PageType)
