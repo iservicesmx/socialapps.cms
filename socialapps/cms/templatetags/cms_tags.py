f1e2bfc60d7c9a69a01a1aa6a26c1afab22ce404
Isaac Torres <istodi@gmail.com>
2012-08-09 14:18:05 -0500

    menu for syllabus/resources

--- socialapps/cms/templatetags/cms_tags.py
+++ socialapps/cms/templatetags/cms_tags.py
@@ -1,31 +1,40 @@
 from django import template
 from permissions.utils import has_permission
-import re
 
 register = template.Library()
 
-@register.inclusion_tag("cms/navigation_portlet.html", takes_context= True)
+
+@register.inclusion_tag("cms/navigation_portlet.html", takes_context=True)
 def show_navigation(context, obj):
-    if not hasattr(obj,'get_local_menu'):
-        while not hasattr(obj,'get_local_menu'):
-            obj = obj.parent.get_type_object()
-    return { 'menu' : obj.get_local_menu(context['user']), 'absolute_url': obj.get_absolute_url()}
-    
+    path_items = context['request'].path.split("/")
+    if "courses" in path_items and ("resources" in path_items or "syllabus" in path_items):
+        section = context['request'].session.get('current_section')
+        if section:
+            obj = section
+    if not hasattr(obj, 'get_local_menu'):
+        while not hasattr(obj, 'get_local_menu'):
+            if obj.parent:
+                obj = obj.parent.get_type_object()
+            else:
+                return None
+    return {'menu': obj.get_local_menu(context['user']), 'absolute_url': obj.get_absolute_url()}
+
+
 @register.inclusion_tag("cms/breadcrumb.html")
 def show_breadcrumb(obj):
     ancestors = obj.get_object_ancestors()[1:]
     anc = []
     if len(ancestors) > 2:
         anc.append(ancestors[0])
-        anc.append(None)        
+        anc.append(None)
         anc.append(ancestors[-1])
     else:
         anc = ancestors
     return {
-        'object'    : obj,
-        'ancestors' : ancestors,
+        'object':       obj,
+        'ancestors':    ancestors,
     }
-    
+
 
 @register.simple_tag(takes_context=True)
 def get_content_parent(context, obj):
@@ -33,7 +42,8 @@ def get_content_parent(context, obj):
         if item.slug in ['syllabus', 'resources', 'discussionroot', 'files']:
             context['content_parent'] = item
     return ""
-            
+
+
 @register.inclusion_tag("cms/multipage_toc.html", takes_context=True)
 def show_multipage_toc(context, obj):
     for item in obj.get_ancestors(include_self=True, ascending=True):
