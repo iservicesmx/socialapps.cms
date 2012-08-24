a91a361005ef7ec5760ee0483feab7857a3d3230
Isaac Torres <istodi@gmail.com>
2012-08-23 21:06:32 -0500

    bug with current_section on resources

--- socialapps/cms/templatetags/cms_tags.py
+++ socialapps/cms/templatetags/cms_tags.py
@@ -7,18 +7,21 @@ register = template.Library()
 @register.inclusion_tag("cms/navigation_portlet.html", takes_context=True)
 def show_navigation(context, obj):
     path_items = context['request'].path.split("/")
+    absolute_url = obj.get_absolute_url()
     if "courses" in path_items and ("resources" in path_items or "syllabus" in path_items):
         section = context['request'].session.get('current_section')
         if section:
-            if section.parent in obj.get_ancestors(True):
-                obj = section
+            if section['section'].parent in obj.get_ancestors(include_self=True, ascending=True):
+                obj = section['section']
+                absolute_url = section['path']
     if not hasattr(obj, 'get_local_menu'):
         while not hasattr(obj, 'get_local_menu'):
             if obj.parent:
                 obj = obj.parent.get_type_object()
+                absolute_url = obj.get_absolute_url()
             else:
                 return None
-    return {'menu': obj.get_local_menu(context['user']), 'absolute_url': obj.get_absolute_url()}
+    return {'menu': obj.get_local_menu(context['user']), 'absolute_url': absolute_url}
 
 
 @register.inclusion_tag("cms/breadcrumb.html")
