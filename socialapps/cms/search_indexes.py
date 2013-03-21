435227c73351fb4fa0494da190cf0e33aba0bd15
Isaac Torres <istodi@gmail.com>
2013-03-21 00:17:13 -0600

    adding haystack

---
+++ socialapps/cms/search_indexes.py
@@ -0,0 +1,14 @@
+import datetime
+from haystack import indexes
+from .models import BaseContent
+
+
+class BaseContentIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+
+    def get_model(self):
+        return BaseContent
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.all()
