c4776cc0478e46438cdbfd0be977da184c2e9e8c
Isaac Torres <istodi@gmail.com>
2013-04-02 13:51:21 -0600

    new search index

--- socialapps/cms/search_indexes.py
+++ socialapps/cms/search_indexes.py
@@ -12,3 +12,6 @@ class BaseContentIndex(indexes.SearchIndex, indexes.Indexable):
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
         return self.get_model().objects.all()
+
+    def get_updated_field(self):
+        return 'modified'
